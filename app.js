6ea60407fcd42e7b35effd63bd6406468854ca23
willswats <william@williamwatson.dev>
2025-02-25 16:54:33 +0000

    feat: add dirname

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 8080;
 
-app.use(express.static('public'));
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
