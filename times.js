0390019f4c4c838e0dd50aa7e9411636958e5c5a
willswats <william@williamwatson.dev>
2025-02-26 14:21:13 +0000

    feat: add timer.js

---
+++ times.js
@@ -0,0 +1,7 @@
+import uuid from 'uuid-random';
+
+const times = [
+  { id: 'wefweeereehererhee', time: '00:00:00' },
+  { id: 'wefweeereerehkmjhe', time: '00:00:00' },
+  { id: 'regegewefweeereehe', time: '00:00:00' },
+];
