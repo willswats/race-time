89feec5df1649e7688a56d29663061e6606857f8
willswats <william@williamwatson.dev>
2025-03-19 12:48:08 +0000

    feat: convert timer to component

---
+++ public/components/timer.js
@@ -0,0 +1,121 @@
+export class RaceTimer extends HTMLElement {
+  constructor() {
+    super();
+
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.milliseconds = 0;
+    this.timeString = '';
+    this.timerId = null;
+
+    this.paragraphTimerText = null;
+    this.buttonStartTimer = null;
+    this.buttonStopTimer = null;
+    this.buttonResetTimer = null;
+    this.buttonSubmitTime = null;
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: 'closed' });
+
+    this.paragraphTimerText = document.createElement('p');
+    this.paragraphTimerText.textContent = this.timeString;
+
+    this.buttonStartTimer = document.createElement('button');
+    this.buttonStartTimer.textContent = 'Start';
+
+    this.buttonStopTimer = document.createElement('button');
+    this.buttonStopTimer.textContent = 'Stop';
+
+    this.buttonResetTimer = document.createElement('button');
+    this.buttonResetTimer.textContent = 'Reset';
+
+    this.buttonSubmitTime = document.createElement('button');
+    this.buttonSubmitTime.textContent = 'Submit';
+
+    this.buttonStartTimer.addEventListener('click', this.startTimer);
+    this.buttonStopTimer.addEventListener('click', this.pauseTimer);
+    this.buttonResetTimer.addEventListener('click', this.resetTimer);
+    this.buttonSubmitTime.addEventListener('click', this.submitTime);
+
+    shadow.append(
+      this.paragraphTimerText,
+      this.buttonStartTimer,
+      this.buttonResetTimer,
+      this.buttonSubmitTime,
+    );
+  }
+
+  updateTimeString() {
+    this.timeString = `${this.hours}:${this.minutes}:${this.seconds}:${this.milliseconds}`;
+  }
+
+  setTimerText() {
+    this.updateTimeString();
+    this.paragraphTimerText.textContent = this.timeString;
+  }
+
+  startTimer() {
+    this.timerId = setInterval(() => {
+      this.milliseconds += 10;
+
+      if (this.minutes === 60) {
+        this.minutes = 0;
+        this.hours += 1;
+      }
+
+      if (this.seconds === 60) {
+        this.seconds = 0;
+        this.minutes += 1;
+      }
+
+      if (this.milliseconds === 1000) {
+        this.milliseconds = 0;
+        this.seconds += 1;
+      }
+
+      this.setTimerText();
+    }, 10);
+  }
+
+  pauseTimer() {
+    clearInterval(this.timerId);
+  }
+
+  resetTimer() {
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.milliseconds = 0;
+    this.setTimerText();
+  }
+
+  async submitTime() {
+    const confirm = window.confirm(
+      'Are you sure you want to submit your time?',
+    );
+    if (confirm) {
+      this.pauseTimer();
+
+      // TODO: fix error 500
+      const payload = { time: this.timeString };
+      console.log('Payload', payload);
+
+      const response = await fetch('times', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+      });
+
+      if (response.ok) {
+        const updatedMessages = await response.json();
+        console.log(updatedMessages);
+      } else {
+        console.log('failed to send message', response);
+      }
+    }
+  }
+}
+
+customElements.define('race-timer', RaceTimer);
