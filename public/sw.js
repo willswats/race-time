341c12ee58ed2ec894b02147d3a4227c5f167b9c
willswats <william@williamwatson.dev>
2025-04-21 16:52:57 +0100

    fix: remove intercept for now

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,27 @@
-/* Log fetch requests and then serve them from the cache */
-function interceptFetch(evt) {
-  evt.respondWith(handleFetch(evt.request));
-  evt.waitUntil(updateCache(evt.request));
-}
-
-/* Retrieve a requested resource from the cache
- * or return a resolved promise if its not there.
- */
-async function handleFetch(request) {
-  const c = await caches.open(CACHE);
-  const cachedCopy = await c.match(request);
-  return cachedCopy || Promise.reject(new Error('no-match'));
-}
-
-/* Invoke the default fetch capability to
- * pull a resource over the network and use
- * that to update the cache.
- */
-async function updateCache(request) {
-  const c = await caches.open(CACHE);
-  const response = await fetch(request);
-  console.log('Updating cache ', request.url);
-  return c.put(request, response);
-}
+// function interceptFetch(evt) {
+//   evt.respondWith(handleFetch(evt.request));
+//   evt.waitUntil(updateCache(evt.request));
+// }
+//
+// /* Retrieve a requested resource from the cache
+//  * or return a resolved promise if its not there.
+//  */
+// async function handleFetch(request) {
+//   const c = await caches.open(CACHE);
+//   const cachedCopy = await c.match(request);
+//   return cachedCopy || Promise.reject(new Error('no-match'));
+// }
+//
+// /* Invoke the default fetch capability to
+//  * pull a resource over the network and use
+//  * that to update the cache.
+//  */
+// async function updateCache(request) {
+//   const c = await caches.open(CACHE);
+//   const response = await fetch(request);
+//   console.log('Updating cache ', request.url);
+//   return c.put(request, response);
+// }
 
 const CACHE = 'race-time';
 const CACHEABLE = [
@@ -45,4 +44,4 @@ async function prepareCache() {
 
 // install the event listener so it can run in the background.
 self.addEventListener('install', prepareCache);
-self.addEventListener('fetch', interceptFetch);
+// self.addEventListener('fetch', interceptFetch);
