c484643c5d898dc652c6b2f2986760e46afeeceb
willswats <william@williamwatson.dev>
2025-04-23 08:34:32 +0100

    feat: change sw

--- public/sw.js
+++ public/sw.js
@@ -23,25 +23,33 @@
 //   return c.put(request, response);
 // }
 
-const CACHE = 'race-time';
-const CACHEABLE = [
-  './',
-  './sw.js',
-  './register-sw.js',
-  './index.html',
-  './manifest.json',
-  './race-results/index.html',
-  './components/race-timer.js',
-  './components/race-results.js',
-];
+// const CACHE = 'race-time';
+// const CACHEABLE = [
+//   './',
+//   './sw.js',
+//   './register-sw.js',
+//   './index.html',
+//   './manifest.json',
+//   './race-results/index.html',
+//   './components/race-timer.js',
+//   './components/race-results.js',
+// ];
 
 /* Prepare and populate a cache. */
-async function prepareCache() {
-  const c = await caches.open(CACHE);
-  await c.addAll(CACHEABLE);
-  console.log('Cache prepared.');
-}
+// async function prepareCache() {
+//   const c = await caches.open(CACHE);
+//   await c.addAll(CACHEABLE);
+//   console.log('Cache prepared.');
+// }
 
 // install the event listener so it can run in the background.
-self.addEventListener('install', prepareCache);
+// self.addEventListener('install', prepareCache);
 // self.addEventListener('fetch', interceptFetch);
+console.log('Service Worker that logs fetch events is now running.');
+
+async function interceptFetch(evt) {
+  console.log(evt.request.method, evt.request.url);
+  return await fetch(evt.request);
+}
+
+self.addEventListener('fetch', interceptFetch);
