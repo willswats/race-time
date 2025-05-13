51f315aa09b08172041eadf18a378cb41ae6bc5a
willswats <william@williamwatson.dev>
2025-05-13 13:36:03 +0100

    feat: remove from sw

--- public/sw.js
+++ public/sw.js
@@ -58,21 +58,3 @@ async function prepareCache() {
 // install the event listener so it can run in the background.
 self.addEventListener('install', prepareCache);
 // self.addEventListener('fetch', interceptFetch);
-//
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
-      }
-      const response = await fetch(e.request);
-      const cache = await caches.open(CACHE);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })(),
-  );
-});
