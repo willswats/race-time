03bc9bda54fbe3431eefb7f3176ad76fd743ead6
willswats <william@williamwatson.dev>
2025-05-14 13:52:32 +0100

    feat: update sw

--- public/sw.js
+++ public/sw.js
@@ -3,31 +3,24 @@ function interceptFetch(evt) {
 
   // Skip interception if the URL contains '/api/'
   if (requestUrl.includes('/api/')) {
-    return; // Let the browser handle this fetch normally
+    return;
   }
 
   evt.respondWith(handleFetch(evt.request));
   evt.waitUntil(updateCache(evt.request));
 }
 
-/* Retrieve a requested resource from the cache
- * or return a resolved promise if its not there.
- */
 async function handleFetch(request) {
-  const c = await caches.open(CACHE);
-  const cachedCopy = await c.match(request);
+  const cache = await caches.open(CACHE);
+  const cachedCopy = await cache.match(request);
   return cachedCopy || Promise.reject(new Error('no-match'));
 }
 
-/* Invoke the default fetch capability to
- * pull a resource over the network and use
- * that to update the cache.
- */
 async function updateCache(request) {
-  const c = await caches.open(CACHE);
+  const cache = await caches.open(CACHE);
   const response = await fetch(request);
   console.log('Updating cache ', request.url);
-  return c.put(request, response);
+  return cache.put(request, response);
 }
 
 const CACHE = 'race-time-v1.0';
@@ -64,7 +57,6 @@ const CACHEABLE = [
   '/components/custom-alert.css',
 ];
 
-/* Prepare and populate a cache. */
 async function prepareCache() {
   const c = await caches.open(CACHE);
   await c.addAll(CACHEABLE);
